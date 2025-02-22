2d8ac667a0f1dbc5bd385657d740e5e780399e6f
xplicitDev <masoodforwork@gmail.com>
2025-02-22 23:20:45 +0530

    search

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Navbar from "./components/navbar";
 import BgGradientAnimated from "./components/bgGradientAnimated";
+import Search from "./components/seearch";
 
 const App = () => {
   return (
