90e48bf83dea58517823d8109a3105e17f799008
xplicitDev <masoodforwork@gmail.com>
2025-02-23 23:15:10 +0530

    L

--- frontEnd/src/components/navbar.jsx
+++ frontEnd/src/components/navbar.jsx
@@ -4,7 +4,7 @@ const navbar = () => {
   return (
     <nav className="fixed top-0 left-0 w-full bg-#000000 bg-opacity-80 text-white z-20">
       <div className="max-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
-        <h1 className="text-left text-2xl font-bold">SoundRanked</h1>
+        <h1 className="text-left text-2xl font-bold">soundRanked</h1>
         <ul className="hidden md:flex space-x-6 text-lg">
           <li>
             <a href="#" className="hover:text-cyan-400">
