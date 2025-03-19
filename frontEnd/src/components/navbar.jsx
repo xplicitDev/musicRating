fa2599eb34dcdd86ee110657824961db8b2a5790
xplicitDev <masoodforwork@gmail.com>
2025-03-19 13:30:12 +0530

    refactor navbar

--- frontEnd/src/components/navbar.jsx
+++ frontEnd/src/components/navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
-const navbar = () => {
+const Navbar = () => {
   const [nav, setNav] = useState(false);
 
   const handleNav = () => {
@@ -9,32 +9,36 @@ const navbar = () => {
   };
 
   return (
-    <div className="fixed top-0 left-0 w-full bg-opacity-15 flex justify-between items-center h-24 px-15 text-white z-30">
+    <div className="fixed top-0 left-0 w-full bg-none bg-opacity-50 backdrop-blur-md flex justify-between items-center h-24 px-6 text-white z-50 shadow-lg">
       <h1 className="text-2xl font-bold text-white">soundRanked</h1>
       <ul className="hidden md:flex">
-        <li className="p-4">Home</li>
-        <li className="p-4">Rankings</li>
-        <li className="p-4">Favorites</li>
-        <li className="p-4">Community</li>
+        <li className="p-4 cursor-pointer hover:text-gray-400">Home</li>
+        <li className="p-4 cursor-pointer hover:text-gray-400">Rankings</li>
+        <li className="p-4 cursor-pointer hover:text-gray-400">Favorites</li>
+        <li className="p-4 cursor-pointer hover:text-gray-400">Community</li>
       </ul>
-      <div onClick={handleNav} className="block md:hidden z-30">
-        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      <div onClick={handleNav} className="block md:hidden z-50 cursor-pointer">
+        {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
       </div>
       <ul
-        className={
-          nav
-            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
-            : "ease-in-out duration-500 fixed left-[-100%]"
-        }
+        className={`fixed top-0 w-[60%] h-full border-r border-gray-900 bg-[#000300] ease-in-out duration-500 z-40 ${
+          nav ? "left-0" : "left-[-100%]"
+        }`}
       >
         <h1 className="text-3xl font-bold text-white m-4">soundRanked</h1>
-        <li className="p-4 border-b border-gray-600">Home</li>
-        <li className="p-4 border-b border-gray-600">Rankings</li>
-        <li className="p-4 border-b border-gray-600">Favorites</li>
-        <li className="p-4">Community</li>
+        <li className="p-4 border-b border-gray-600 cursor-pointer hover:text-gray-400">
+          Home
+        </li>
+        <li className="p-4 border-b border-gray-600 cursor-pointer hover:text-gray-400">
+          Rankings
+        </li>
+        <li className="p-4 border-b border-gray-600 cursor-pointer hover:text-gray-400">
+          Favorites
+        </li>
+        <li className="p-4 cursor-pointer hover:text-gray-400">Community</li>
       </ul>
     </div>
   );
 };
 
-export default navbar;
+export default Navbar;
